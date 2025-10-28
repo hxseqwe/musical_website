83ff53f6b44d32044fa2422f436a19bc066bd057
hxseqwe <artem1162006@mail.ru>
2025-10-28 06:19:33 +0400

    key

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'fallback-key-change-in-production'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(24)
     
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
